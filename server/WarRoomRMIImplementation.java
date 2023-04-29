ab0cc6db403894c0ca705de0e6dcd9d05cac8226
IsaacAmann <isaacmnn@gmail.com>
2023-04-29 19:54:15 -0400

    Added code to allow server to register on different address by passing it as a command line argument

--- server/WarRoomRMIImplementation.java
+++ server/WarRoomRMIImplementation.java
@@ -141,7 +141,7 @@ public class WarRoomRMIImplementation extends UnicastRemoteObject implements War
 		//The return array only includes the chats the client has not seen yet
 		//int i = chatMessages.size() - totalChats - 1;
 		String[] newChats = new String[chatMessages.size() - totalChats];
-		System.out.println("getchats");
+		//System.out.println("getchats");
 		for(int i = 0; i < newChats.length; i++)
 		{
 			newChats[i] = chatMessages.get((chatMessages.size() - 1)-i);
--- server/WarRoomServer.java
+++ server/WarRoomServer.java
@@ -1,7 +1,7 @@
-
 import java.rmi.registry.*;
 import java.rmi.*;
 import java.util.*;
+import java.util.Properties;
 
 public class WarRoomServer
 {
@@ -9,12 +9,22 @@ public class WarRoomServer
 	
 	public static void main(String[] args)
 	{
+		if(args.length == 1)
+		{
+			Properties systemProperties = System.getProperties();
+			systemProperties.put("java.rmi.server.hostname", args[0]);
+			System.setProperties(systemProperties);
+			System.out.println("Registering on " + args[0]);
+		}
+		else
+		{
+			System.out.println("Registering on localhost");
+		}
 		try
 		{
 			WarRoomServerInterface RMIServer = new WarRoomRMIImplementation();
 			Registry registry = LocateRegistry.createRegistry(PORT);
 			registry.rebind("WarRoomRMIImplementation", RMIServer);
-			
 		}
 		catch(Exception e)
 		{
